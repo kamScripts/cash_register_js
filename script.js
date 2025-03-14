536be659096ff04c11c010aa965e1b443c3d3edf
GeKamB <k.gebski1@gmail.com>
2025-03-14 19:46:32 +0000

    add vars, selectors

---
+++ script.js
@@ -0,0 +1,17 @@
+const cash = document.getElementById('cash');
+const purchaseBtn = document.getElementById('purchase-btn');
+const changeDue = document.getElementById('change-due');
+const priceDisplay = document.getElementById('price-val');
+
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
